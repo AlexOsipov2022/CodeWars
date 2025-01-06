979170afdaa81bbcbd77bce649b2f4e7dfa9af63
alexandr-osipov <alexandr.osipov@idfinance.com>
2025-01-06 17:11:53 +0300

    CodeWars

---
+++ src/Leetcode/PalindromeNumber.java
@@ -0,0 +1,20 @@
+package Leetcode;
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+        int reversed = 0;
+        int original = x;
+
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return (x == reversed) || (x == reversed / 10);
+    }
+}
+
+
+//https://leetcode.com/problems/palindrome-number/description/
